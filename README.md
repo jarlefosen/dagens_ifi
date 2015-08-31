9b2340d36cdb05899340e6877b5e8ad0c8180756
Jarle Fosen <jarlefosen@gmail.com>
2015-08-31 22:07:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# IFI dinner
+
+Check this weeks dinner with the commando line
+
+`$ ./dagens_ifi.py`
+
+## Setup
+
+`$ chmod +x dagens_ifi.py`
+
+That's it.
---
+++ dagens_ifi.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# Created by Jarle Fosen - jarlefosen@gmail.com
+import json
+import urllib2
+
+url = "https://www.kimonolabs.com/api/1zpgdth4?kimmodify=1"
+data = json.load(urllib2.urlopen(url))["results"]
+
+week = data["menu_date"]
+menu = data["menu_grouped"]
+
+print week["week_name"]
+print week["week_date"]
+
+for day in menu:
+    print ""
+    print day["day_of_week"]
+    for dish in day["dishes"]:
+        print "\t", dish["type"], "-", dish["name"]
